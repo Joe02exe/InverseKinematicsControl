c59fb59030c795ccd9a90e12e2b1872bcfb6dbd6
Joe02exe <lonza.johannes@gmail.com>
2024-01-24 14:26:53 +0100

    fixed shellscript

---
+++ run_coppelia.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sleep 10
+# Use the argument directly in your runcoppelia command
+runcoppelia "$1"
--- scripts/coppelia.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-scene_file = "scene.ttt"
-command = f'runcoppelia {scene_file}'
-
-subprocess.run(command, shell=True)
